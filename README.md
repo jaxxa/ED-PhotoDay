7241bab4a78a34ccdfb519c462e0e911dbeaaff1
Jaxxa <jaxxa90@gmail.com>
2017-04-05 08:09:47 +1000

    Initial Prototype with Hardcoded Time

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ Store each time as Day / Hour (not ticks since last time).
 
 Configure multiple Times, Daily at 9 am, and 9 pm?
 Configure every X days?
+
+#Change Log
+
+01.00.00
+*Initial Release
---
+++ Source/ED-PhotoDay/ED-PhotoDay/MapComponent_PhotoDay.cs
@@ -0,0 +1,43 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace EnhancedDevelopment.Example.ED_PhotoDay
+{
+    class MapComponent_PhotoDay : Verse.MapComponent
+    {
+
+        public const int CHECK_INTERVAL_TICKS = 10000;
+
+        public MapComponent_PhotoDay(Map map) : base(map)
+        {
+
+        }
+
+
+        public override void MapComponentTick()
+        {
+            Log.Message("MapComponent_PhotoDay.MapComponentTick");
+
+            if (Find.TickManager.TicksGame % CHECK_INTERVAL_TICKS != 0)
+            {
+                return;
+            }
+
+            Letter _Letter = new Letter("Photo Day","Look your best, its Photo Day", LetterType.Good);
+
+            Find.LetterStack.ReceiveLetter(_Letter,"PhotoDay Letter");
+
+            if (!Find.TickManager.Paused)
+            {
+                Find.TickManager.TogglePaused();
+            }
+        }
+
+    }
+}
