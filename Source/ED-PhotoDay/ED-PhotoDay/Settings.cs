31917890bab7f8fdb5be52300547c9d450d45636
Jaxxa <jaxxa90@gmail.com>
2017-04-05 08:25:17 +1000

    Starting HugsLib settings implimentation.

---
+++ Source/ED-PhotoDay/ED-PhotoDay/Settings.cs
@@ -0,0 +1,19 @@
+﻿using HugsLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EnhancedDevelopment.Example.ED_PhotoDay
+{
+    class Settings : ModBase
+    {
+        public override string ModIdentifier {
+            get { return "ED-PhotoDay"; }
+        }
+
+
+
+    }
+}
+ 
