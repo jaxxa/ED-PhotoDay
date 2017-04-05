4e1312fa6823e7de1d6b677ff1143527ae041907
Jaxxa <jaxxa90@gmail.com>
2017-04-05 17:57:55 +1000

    Converting to use Hugs ModBase

--- Source/ED-PhotoDay/ED-PhotoDay/MapComponent_PhotoDay.cs
+++ Source/ED-PhotoDay/ED-PhotoDay/MapComponent_PhotoDay.cs
@@ -1,43 +1,43 @@
-﻿using RimWorld;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using UnityEngine;
-using Verse;
+﻿//using RimWorld;
+//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
+//using System.Text;
+//using UnityEngine;
+//using Verse;
 
-namespace EnhancedDevelopment.Example.ED_PhotoDay
-{
-    class MapComponent_PhotoDay : Verse.MapComponent
-    {
+//namespace EnhancedDevelopment.Example.ED_PhotoDay
+//{
+//    class MapComponent_PhotoDay : Verse.MapComponent
+//    {
 
-        public const int CHECK_INTERVAL_TICKS = 10000;
+//        public const int CHECK_INTERVAL_TICKS = 10000;
 
-        public MapComponent_PhotoDay(Map map) : base(map)
-        {
+//        public MapComponent_PhotoDay(Map map) : base(map)
+//        {
 
-        }
+//        }
 
 
-        public override void MapComponentTick()
-        {
-            Log.Message("MapComponent_PhotoDay.MapComponentTick");
+//        public override void MapComponentTick()
+//        {
+//            Log.Message("MapComponent_PhotoDay.MapComponentTick");
 
-            if (Find.TickManager.TicksGame % CHECK_INTERVAL_TICKS != 0)
-            {
-                return;
-            }
+//            if (Find.TickManager.TicksGame % CHECK_INTERVAL_TICKS != 0)
+//            {
+//                return;
+//            }
 
-            Letter _Letter = new Letter("Photo Day","Look your best, its Photo Day", LetterType.Good);
+//            Letter _Letter = new Letter("Photo Day","Look your best, its Photo Day", LetterType.Good);
 
-            Find.LetterStack.ReceiveLetter(_Letter,"PhotoDay Letter");
+//            Find.LetterStack.ReceiveLetter(_Letter,"PhotoDay Letter");
 
-            if (!Find.TickManager.Paused)
-            {
-                Find.TickManager.TogglePaused();
-            }
-        }
+//            if (!Find.TickManager.Paused)
+//            {
+//                Find.TickManager.TogglePaused();
+//            }
+//        }
 
-    }
-}
+//    }
+//}
--- Source/ED-PhotoDay/ED-PhotoDay/Settings.cs
+++ Source/ED-PhotoDay/ED-PhotoDay/Settings.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Verse;
 
 namespace EnhancedDevelopment.Example.ED_PhotoDay
 {
@@ -12,6 +13,28 @@ namespace EnhancedDevelopment.Example.ED_PhotoDay
             get { return "ED-PhotoDay"; }
         }
 
+
+
+        public const int CHECK_INTERVAL_TICKS = 10000;
+        public override void Tick(int currentTick)
+        {
+            base.Tick(currentTick);
+
+            if (Find.TickManager.TicksGame % CHECK_INTERVAL_TICKS != 0)
+            {
+                return;
+            }
+
+            Letter _Letter = new Letter("Photo Day - H", "Look your best, its Photo Day - H", LetterType.Good);
+
+            Find.LetterStack.ReceiveLetter(_Letter, "PhotoDay Letter");
+
+            if (!Find.TickManager.Paused)
+            {
+                Find.TickManager.TogglePaused();
+            }
+
+        }
 
 
     }
